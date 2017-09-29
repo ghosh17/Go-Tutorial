f319c6abfb35b0e044d1cee0246228adda36ee81
Adit Ghosh <ghosh17@purdue.edu>
2017-09-29 11:51:42 -0700

    Bank Account concurrent Transactions
    Bank Account that has concurrent Transactions. You can concurrently deposite and withdraw money

--- Bank_Account.go
+++ Bank_Account.go
@@ -7,8 +7,9 @@ import (
 )
 
 var wg = sync.WaitGroup{}
+var wg2 = sync.WaitGroup{}
 
-var Balance float64 = 50.00
+var Balance float64 = 100000.00
 
 func main() {
 	//Opening & Closing = Open and close channel
@@ -16,18 +17,14 @@ func main() {
 	//Withdrawal $$ <- account
 
 	wg.Add(2)
-	c := make(chan float64, 50) //buffer
-	dflag := 0
-	d := 123406.78
-	w := 0.00
-	if d > 0 {
-		dflag = 1
-	}
-	money := d - w
+	c := make(chan float64)
+
+	deposite := 23456.79
+	withdraw := 0.01
 
 	go func() {
 		for i := range c {
-			time.Sleep(time.Duration(3 * time.Second))
+			time.Sleep(time.Duration(5 * time.Second))
 			fmt.Printf("You got $%0.2f M's in your bank account", i)
 			wg.Done()
 		}
@@ -35,13 +32,15 @@ func main() {
 	}()
 
 	go func() {
+		wg2.Add(2)
 
-		if dflag == 1 {
-			c <- (Balance + money)
-		} else {
-			c <- (Balance - money)
-		}
+		go Deposite(c, deposite)
+		//c <- (Balance + money)
 
+		go Withdraw(c, withdraw)
+		//c <- (Balance + money)
+
+		wg2.Wait()
 		wg.Done()
 	}()
 
@@ -49,4 +48,18 @@ func main() {
 
 	wg.Wait()
 
+}
+
+func Deposite(c chan float64, deposite float64) {
+	fmt.Printf("Depositing: %0.2f\n", deposite)
+	Balance = Balance + deposite
+	c <- (Balance)
+	wg2.Done()
+}
+
+func Withdraw(c chan float64, withdraw float64) {
+	fmt.Printf("Withdrawing: %0.2f\n", withdraw)
+	Balance = Balance - withdraw
+	c <- (Balance)
+	wg2.Done()
 }
